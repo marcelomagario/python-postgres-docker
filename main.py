c364deffd6254e5b893a73b935015ab40f629168
Marcelo <marcelomagario@gmail.com>
2023-03-21 15:54:38 -0300

    Begining the project

---
+++ main.py
@@ -0,0 +1,48 @@
+import pandas as pd 
+import numpy as np
+from IPython.display import display
+from sqlalchemy import create_engine
+import psycopg2 
+
+engine = create_engine('postgresql+psycopg2://user:password@localhost:5432/nomeDoBanco')
+
+
+# making data frame 
+df = pd.read_excel("registro_de_usuarios.xlsx") 
+# df_investor = pd.read_excel("files/PN_2012-03-15.xlsx", skiprows=6) 
+
+
+
+
+# function to import Excel to Table tb_usuario from Postgres Database  
+def importToDatabase():
+    df = pd.read_excel("registro_de_usuarios.xlsx") 
+    df.to_sql(name='tb_usuario', con=engine, if_exists='append', index=False)
+    # pd.ExcelFile('files/PN_2012-03-15.xlsx')
+
+
+
+def main():
+    while True:
+        print('-' * 90)
+        print('Projeto usando o Python/Pandas, Postgres e Docker/Docker Compose')
+        print('-' * 90)
+        print()
+        print('Opções: ')
+        print()
+        print('[1] - Fazer o upload do Excel, salvando os registros no banco Postgres')
+        print('[0] - Sair')
+
+
+        option = int(input('Digite a sua opção: '))
+        if option == 1:
+            importToDatabase() 
+        elif option == 0:
+            break
+        else:
+            print('Opção inválida')
+            main()
+            print()
+        
+print()
+main()
---
+++ usuario.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.tb_usuario
+(
+    "ID" text,
+    "Nome" text,
+    "Sobrenome" text,
+    "Email" text,
+    "Celular" text
+);
+
